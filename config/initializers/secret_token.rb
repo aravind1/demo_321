e7f1f5862cdf3577b6542f56c324321002ac5e73
Aravind <aravind.ror@gmail.com>
2012-05-25 11:42:49 +0530

    jhgjk git commit -m jhgjk exit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo321::Application.config.secret_token = 'a4c84929c7f9f92709ae664e73c1b80638f87583d378a9a9d1a4d9768df70be404882e4bcdd769d32733b51846bce4b3c25902d9ef2815ec4777eb417856f557'
